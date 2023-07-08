e7c755eb966ee7e89cb5940c31967d7b8f244904
Christina <kristia1ewik@gmail.com>
2023-07-08 19:33:16 +0300

    Updated home task, used my own logic.

--- src/HT6.java
+++ src/HT6.java
@@ -6,17 +6,35 @@ public class HT6 {
     public HashSet findPairsOfLitera(String [] arr){
         ArrayList<String> pairedWords = new ArrayList<String>(  );
         HashSet<Character> resultList = new HashSet<Character>(  );
+
         for (int i = 0; i<arr.length; i++){
-            char[] charArray = arr[i].toCharArray();
-            HashSet <Character> charsList = new HashSet<Character>();
-            for( int j = 0; j<charArray.length;j++){
-                if (!charsList.contains(charArray[j])){
-                    charsList.add(charArray[j]);
+            if (arr[i].length()>2){
+
+                HashSet<Integer> contCharactrs = new HashSet<Integer>(  );
+                char[] charArray = arr[i].toCharArray();
+                HashSet <Character> charsList = new HashSet<Character>();
+
+                for( int j = 0; j<charArray.length;j++){
+                    if (!charsList.contains(charArray[j])){
+                        charsList.add(charArray[j]);
+                    }
+                }
+                for (Character s: charsList){
+                    int counter = 0;
+
+                    for (Character t: charArray){
+                        if (s.equals(t)){
+                            counter++;
+                        }
+                    }
+                    contCharactrs.add(counter);
+                }
+
+                if (contCharactrs.size()==1 && pairedWords.size()<2){
+                    pairedWords.add(arr[i]);
                 }
             }
-            if ((arr[i].length()%charsList.size()==0) && (arr[i].length()>charsList.size())){
-                pairedWords.add(arr[i]);
-            }
+
         }
         for(String i:pairedWords){
             char[] charsSegmentation  = i.toCharArray();
@@ -24,7 +42,7 @@ public class HT6 {
                 resultList.add(charsSegmentation[k]);
             }
         }
-        System.out.println("Слова у яких кожна літера трапляється парну кількість разів:" + pairedWords);
+        System.out.println("Перші два, в яких кожен символ трапляється парну кількість разів:" + pairedWords);
         System.out.println("Набір унікальних символів у словах: "+ resultList);
         return resultList;
     }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 
 public class Main {
     public static void main(String[] args) {
-        String [] inputArr = { "мама", "тато", "їж їжак желе","hi","нан","рарара","папа","та","бабак","арра"};
+        String [] inputArr = {"abacab","мама", "тато", "їж їжак желе","hi","нан","рарара","папа","та","бабак","арра"};
         HT6 newHt = new HT6();
         newHt.findPairsOfLitera(inputArr);
     }
 }
 
+//"abacab","мама", "тато", "їж їжак желе","hi","нан","рарара","папа","та","бабак","арра"
